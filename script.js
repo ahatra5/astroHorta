afcf25285d561c8f98b0211a7040286c3b064391
ahatra5 <joao03richt@gmail.com>
2024-02-02 00:47:38 -0300

    inícioCarrossel

---
+++ script.js
@@ -0,0 +1,33 @@
+let currentIndex = 0;
+const totalSlides = 4;
+const carrossel = document.querySelector('.carrossel');
+const images = document.querySelectorAll('.carrossel-img');
+
+function changeSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    updateCarrossel();
+}
+
+function updateCarrossel() {
+    const translateValue = -currentIndex * 100 + '%';
+    carrossel.style.transform = 'translateX(' + translateValue + ')';
+}
+
+// Adicionando um evento de transição para verificar quando a transição termina
+carrossel.addEventListener('transitionend', () => {
+    // Exibindo a imagem atual
+    images[currentIndex].style.opacity = '1';
+});
+
+// Definindo a opacidade inicial da primeira imagem
+images[0].style.opacity = '1';
+
+// Chamando a função updateCarrossel inicialmente para exibir a primeira imagem
+updateCarrossel();
