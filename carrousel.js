644684c2081a3cdd317e91125a36e2e92ef4d501
ahatra5 <joao03richt@gmail.com>
2024-04-05 10:15:33 -0300

    svr

--- carrousel.js
+++ carrousel.js
@@ -1,38 +1,23 @@
-let currentIndex = 0;
+let slideAtual = 0;
 const totalSlides = 4;
 const carrossel = document.querySelector('.carrossel');
 const images = document.querySelectorAll('.carrossel-img');
 
-function changeSlide(direction) {
-    currentIndex += direction;
-
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-        currentIndex = 0;
-    }
-
-    updateCarrossel();
-}
-
-function updateCarrossel() {
-    const translateValue = -currentIndex * 100 + '%';
+function reiniciar() {
+    const translateValue = -slideAtual * 100 + '%';
     carrossel.style.transform = 'translateX(' + translateValue + ')';
 }
 
-// Adicionando um evento de transição para verificar quando a transição termina
-carrossel.addEventListener('transitionend', () => {
-    // Exibindo a imagem atual
-    images[currentIndex].style.opacity = '1';
-});
 
+function changeSlide(direction) {
+    slideAtual += direction;
 
-// Definindo a opacidade inicial da primeira imagem
-images[0].style.opacity = '1';
-images[1].style.opacity = '1';
-images[2].style.opacity = '1';
-images[3].style.opacity = '1';
+    if (slideAtual <= 0) {
+        slideAtual = totalSlides + - 1;
+    } else if (slideAtual >= totalSlides) {
+        slideAtual = 0;
+    }
 
-// Chamando a função updateCarrossel inicialmente para exibir a primeira imagem
-updateCarrossel();
+    reiniciar();
+}
 
